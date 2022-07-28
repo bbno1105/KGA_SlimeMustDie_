27f3fe6b560313e819a608de909dae61a8a9db7e
BBNO <bbno1105@gmail.com>
2022-07-28 21:58:11 +0900

    가시함정 스케일 조정, 카메라 벽 뚫지 못하도록 수정
    ▶ 7/28   함정  :  가시함정 스케일 조정
    ▶ 7/28   카메라  :  카메라 벽 뚫지 못하도록 수정

--- KGA_UnityProject/Assets/0) Scripts/Player/CameraMove.cs
+++ KGA_UnityProject/Assets/0) Scripts/Player/CameraMove.cs
@@ -9,16 +9,45 @@ public class CameraMove : MonoBehaviour
     public void SetHP(float _CamAngleSpeed) { this.CamAngleSpeed = _CamAngleSpeed; }
 
     GameObject camera;
+    Vector3 cameraPOS;
+
+    public GameObject TestPlayer;
+    public Renderer[] TestRenderer;
+
+    Color color;
+
+    float dist;
 
     void Start()
     {
         camera = this.transform.GetChild(0).gameObject;
+        dist = (this.transform.position - camera.transform.position).magnitude;
+        cameraPOS = camera.transform.position;
+
+        TestRenderer = TestPlayer.GetComponentsInChildren<Renderer>();
     }
 
     void Update()
     {
         LookAround();
         ObstacleMove();
+
+        Vector3 cameraD = this.transform.position - camera.transform.position;
+        if (cameraD.magnitude < 1.6f)
+        {
+            for (int i = 0; i < TestRenderer.Length; i++)
+            {
+                // TestRenderer[i].material.color
+            }
+        }
+        else
+        {
+            for (int i = 0; i < TestRenderer.Length; i++)
+            {
+                TestRenderer[i].enabled = true;
+            }
+        }
+
     }
 
     public void LookAround()
@@ -44,6 +73,21 @@ public class CameraMove : MonoBehaviour
 
     void ObstacleMove()
     {
+        RaycastHit hit;
+        if(Physics.Raycast(this.transform.position,(camera.transform.position-this.transform.position).normalized, out hit, dist))
+        {
+            if (hit.transform.gameObject != camera && hit.transform.gameObject.tag != "Monster" && hit.transform.gameObject.tag != "DamagedMonster" && hit.transform.gameObject.tag != "Player")
+            {
+                camera.transform.position = hit.point;
+            }
+        }
+        else
+        {
+            camera.transform.localPosition = Vector3.Lerp(camera.transform.localPosition, cameraPOS, Time.deltaTime * 5f);
+        }
 
+        if((this.transform.position - camera.transform.position).magnitude < 0.1f)
+        {
+        }
     }
 }
